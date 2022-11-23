f8773f554f14e6652a4aaf70eb78c7cfc1779756
minhlang102 <minhlangstudy@gmail.com>
2022-11-23 17:36:02 +0700

    v1

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from lane_detect import detect_image, detect_video
 ##################### EXECUTE CODE #############################
 image = cv2.imread('./img/lane.jpg')
 cap = cv2.VideoCapture('./video/test4.webm')
-detect_image(image)
+# detect_image(image)
 detect_video(cap)
